d60cbc9cae861e37f085ba834d327231defe2684
TriLLuxe <glazunovkd@gmail.com>
2025-04-16 10:21:34 +0000

    08 v1

---
+++ module3/08/semaphore.h
@@ -0,0 +1,15 @@
+#ifndef SEMAPHORE_H
+#define SEMAPHORE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+void sem_lock(int sem_id);
+void sem_unlock(int sem_id);
+
+#endif 
